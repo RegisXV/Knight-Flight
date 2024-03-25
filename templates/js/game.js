46973eacc85993d9eaaea7af090d7275f69f2d92
RegisXV <Kyle.mercadov@gmail.com>
2024-03-24 20:09:11 -0400

    added ground and physics

--- templates/js/game.js
+++ templates/js/game.js
@@ -1,13 +1,19 @@
 let gameScene = new Phaser.Scene('Game');
 
+
+
 gameScene.preload = function(){
     this.load.image('background',"assets/oak_woods_v1.0/oak_woods_v1.0/background/background_layer_1.png");
-    this.load.image('ground',"assets")
+    this.load.image('ground',"assets/Starter Tiles Platformer/SwampSetGrid.png");
+    this.load.tilemapTiledJSON('tilemap', 'assets/Starter Tiles Platformer/Ground.json');
     this.load.spritesheet('knight_idle',"assets/FreeKnight_v1/Colour1/Outline/120x80_PNGSheets/_Idle.png", { frameWidth: 120, frameHeight: 80 });
     this.load.spritesheet('knight_run',"assets/FreeKnight_v1/Colour1/Outline/120x80_PNGSheets/_Run.png", { frameWidth: 120, frameHeight: 80 });
+    this.load.spritesheet('knight_jump',"assets/FreeKnight_v1/Colour1/Outline/120x80_PNGSheets/_Jump.png", { frameWidth: 120, frameHeight: 80 });
+    this.load.spritesheet('knight_fall',"assets/FreeKnight_v1/Colour1/Outline/120x80_PNGSheets/_Fall.png", { frameWidth: 120, frameHeight: 80 });
     //this.load.spritesheet('fire', "assets/fire_fx_v1.0/fire_fx_v1.0/png/blue/loops/burning_loop_1.png",{frameWidth: 120, frameHeight: 80});
     console.log("Preload finished");
 };
+
 
 gameScene.create = function(){
     let background = this.add.sprite(0, 0, 'background'); // Set position to (0, 0)
@@ -19,11 +25,18 @@ gameScene.create = function(){
 
      background.setOrigin(0); // Set origin to top-left corner
     background.setPosition(0, 0); // Set position to (0, 0)
-    
-    this.player = this.physics.add.sprite(50, 250, 'knight_idle');
 
+    this.cursors = this.input.keyboard.createCursorKeys();
 
+    const map = this.make.tilemap({key: 'tilemap' });
+    const tileset = map.addTilesetImage('Forest tiles', 'ground');
     
+    const ground = map.createLayer('Tile Layer 1', tileset);
+    ground.setCollisionByProperty({ collide: true});
+
+    this.matter.world.convertTilemapLayer(ground);
+    ground.setPosition(0, game.config.height - ground.height);
+
 
     this.anims.create({
         key: 'idle',
@@ -37,13 +50,52 @@ gameScene.create = function(){
         frameRate: 10,
         repeat: -1
     });
-    this.player.anims.play('idle', true);
+    this.anims.create({
+        key: 'jump',
+        frames: this.anims.generateFrameNumbers('knight_jump', { start: 0, end: 2 }), // Adjust frame range accordingly
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'fall',
+        frames: this.anims.generateFrameNumbers('knight_jump', { start: 0, end: 2 }), // Adjust frame range accordingly
+        frameRate: 10,
+        repeat: -1
+    });
+   this.knight = this.matter.add.sprite(50,250, 'knight_idle')
+        .play('idle');
+        this.knight.setBody({
+            type: 'rectangle',
+            width: 40, // Adjust width and height according to your knight sprite
+            height: 70,
+            density: 0.5,
+            friction: 0.5,
+            restitution: 0.2
+        });
+        this.knight.setFixedRotation();
 
     console.log("Create finished");
 };
 
-gameScene.update = function(){
-   
+
+gameScene.update = function()
+{
+    const speed = 1.25;
+
+   if(this.cursors.left.isDown){
+    this.knight.play('run')
+    this.knight.setVelocityX(-speed)
+    
+   }
+   else if (this.cursors.right.isDown){
+    this.knight.play('run')
+    this.knight.setVelocityX(speed)
+   }
+   else
+   {
+    this.knight.setVelocityX(0)
+    this.knight.play('idle')
+   }
 };
 
 let config = {
@@ -52,9 +104,9 @@ let config = {
     height: 360,
     scene: gameScene,
     physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 },
+        default: 'matter',
+        matter: {
+            gravity: { y: 1 },
             debug: false
         }
     }
