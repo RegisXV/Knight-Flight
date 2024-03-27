579827791a63dd7603bd435b933c1775dc62e872
RegisXV <Kyle.mercadov@gmail.com>
2024-03-27 18:41:30 -0400

    working coins tile changes and player spawn point

--- templates/js/game.js
+++ templates/js/game.js
@@ -5,6 +5,7 @@ let gameScene = new Phaser.Scene('Game');
 gameScene.preload = function(){
     this.load.image('background',"assets/oak_woods_v1.0/oak_woods_v1.0/background/background_layer_1.png");
     this.load.image('ground',"assets/Starter Tiles Platformer/BasicGreenGrid.png");
+    this.load.image('coin',"assets/Starter Tiles Platformer/coin.png");
     this.load.tilemapTiledJSON('tilemap', 'assets/Starter Tiles Platformer/Ground.json');
     this.load.atlas('knight', 'assets/knight.png', 'assets/knight.json');
     //preload finished
@@ -68,6 +69,11 @@ gameScene.create = function(){
 
     background.setOrigin(0); // Set origin to top-left corner
     background.setPosition(0, 0); // Set position to (0, 0)
+    // Create world bounds and set their label
+    let topWall = this.matter.add.rectangle(this.sys.game.config.width / 2, -10, this.sys.game.config.width, 20, { isStatic: true, label: 'World Bounds' });
+    let bottomWall = this.matter.add.rectangle(this.sys.game.config.width / 2, this.sys.game.config.height + 10, this.sys.game.config.width, 20, { isStatic: true, label: 'World Bounds' });
+    let leftWall = this.matter.add.rectangle(-10, this.sys.game.config.height / 2, 20, this.sys.game.config.height, { isStatic: true, label: 'World Bounds' });
+    let rightWall = this.matter.add.rectangle(this.sys.game.config.width + 10, this.sys.game.config.height / 2, 20, this.sys.game.config.height, { isStatic: true, label: 'World Bounds' });
 
     this.cursors = this.input.keyboard.createCursorKeys(); //Getting input from keyboard
 
@@ -77,18 +83,63 @@ gameScene.create = function(){
     const ground = map.createLayer('Layer', tileset)
     ground.setCollisionByProperty({ collide: true });
 
+
     this.matter.world.convertTilemapLayer(ground);
 
-    this.knight = this.matter.add.sprite(40,245,'knight')
-        .setOrigin(0.5,0.5);
-    this.knight.setBody({ //hitbox for knight
-        type: 'rectangle',
-        width: 20,
-        height: 43,
-        allowRotation: false
-    })
-    this.knight.body.position.y -=18; //This code centers hitbox on knights body
-    this.knight.body.position.x +=3;
+    const objectsLayer = map.getObjectLayer('objects')
+
+    objectsLayer.objects.forEach(objData => {
+        const{x,y,name} = objData
+        switch(name)
+        {
+        case 'knightspawn':
+        {
+            this.knight = this.matter.add.sprite(x,y,'knight')
+            .setOrigin(0.5,0.5);
+            this.knight.setBody({ //hitbox for knight
+                type: 'rectangle',
+                width: 20,
+                height: 43,
+                allowRotation: false
+            })
+        this.knight.body.position.y -=18; //This code centers hitbox on knights body
+        this.knight.body.position.x +=3;
+        this.knight.setFixedRotation(true);
+        this.cameras.main.startFollow(this.knight);
+        this.matter.world.on('collisionstart', (event, pair) => {
+            // Check if the knight is one of the bodies and if it collided with the bottom wall
+            if ((pair.bodyA === this.knight.body && pair.bodyB === bottomWall.body) || 
+                (pair.bodyB === this.knight.body && pair.bodyA === bottomWall.body)) {
+                // Destroy the knight
+                this.knight.destroy();
+            
+                // Respawn the knight at the original position
+                this.knight = this.matter.add.sprite(x, y, 'knight');
+            }
+        });
+        
+    
+        this.knight.setOnCollide((data) => {
+            this.isTouching = true
+            
+        });
+        }
+        case 'coin':
+            {
+                const coin = this.matter.add.sprite(x,y,'coin')
+                .setOrigin(0.5,0.5);
+                coin.setBody({
+                    type: 'circle',
+                    radius: 10
+                });
+                coin.setStatic(true);
+                coin.setSensor(true);
+                coin.setOnCollide(() => {
+                    coin.destroy();
+                });
+            }    
+        }
+    });
 
     this.knight.setFixedRotation(true);
 
@@ -96,10 +147,7 @@ gameScene.create = function(){
 
     this.cameras.main.setZoom(1.5);
 
-    this.knight.setOnCollide((data) => {
-        this.isTouching = true
-        
-    });
+   
     
 
 
@@ -138,6 +186,7 @@ gameScene.update = function()
     }
     }
    
+    
 };
 
 let config = {
